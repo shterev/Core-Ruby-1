69b27145b5fd6603e6dc0ed52f99622125de3748
Yordan Shterev <shterew@gmail.com>
2014-11-04 18:23:49 +0200

    Resolve task for hashes

--- week1/2-hashes/solution.rb
+++ week1/2-hashes/solution.rb
@@ -1,3 +1,19 @@
 class Hash
-  # Your code goes here.
+  def pick(*keys)
+    valid_keys = self.keys & keys
+    Hash[valid_keys.map { |key| [key, self[key]] }]
+  end
+
+  def except(*keys)
+    valid_keys = self.keys - keys
+    Hash[valid_keys.map { |key| [key, self[key]] }]
+  end
+
+  def compact_values
+    self.select { |k, v| v }
+  end
+
+  def defaults(hash)
+    hash.merge(self)
+  end
 end
--- week1/2-hashes/solution_test.rb
+++ week1/2-hashes/solution_test.rb
@@ -3,7 +3,20 @@ require 'minitest/autorun'
 require_relative 'solution'
 
 class HashTest < Minitest::Test
-  def test_the_truth
-    assert true
+  def test_pick
+    assert_equal({a: 1, b: 2, c: 3}.pick(:a, :b), {a: 1, b: 2})
   end
+
+  def test_except
+    assert_equal({a: 1, b: 2, d: nil}.except(:d), {a: 1, b: 2})
+  end
+
+  def test_except
+    assert_equal({a: 1, b: 2, c: false, d: nil}.compact_values, {a: 1, b: 2})
+  end
+
+  def test_defaults
+    assert_equal({a: 1, b: 2,}.defaults(a: 4, c: 3), {a: 1, b: 2, c: 3})
+  end
+
 end
